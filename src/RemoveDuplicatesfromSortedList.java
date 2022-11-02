a1a965b3973405c56f7639baac2fa50f7f706af4
shariorfarhan07 <md.sharior.hossain.farhan@g.bracu.ac.bd>
2022-11-03 03:11:36 +0600

    Day 6

--- src/RemoveDuplicatesfromSortedList.java
+++ src/RemoveDuplicatesfromSortedList.java
@@ -2,18 +2,30 @@ public class RemoveDuplicatesfromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode left=head;
         ListNode right=head.next;
-        while (right!=null){
-            if (left.val== right.val){
+        while (left!=null){
+//            System.out.println(left.val+" "+ right.val);
+            while (right!=null&&left.val== right.val){
                 left.next=right.next;
+                right=right.next;
             }
-            left=left.next;
-            right=right.next;
+                left=left.next;
+            if (right==null)
+                break;
         }
         return head;
     }
 
     public static void main(String[] args) {
         ListNode l=new  ListNode(1,new  ListNode(1,new  ListNode(2,new  ListNode(2,null))));
+        l=new RemoveDuplicatesfromSortedList().deleteDuplicates(l);
+        print(l);
+//        for (int i = 0; i < 50; i=i+2) {
+//            System.out.println(i);
+//        }
+  }
+    public static void print(ListNode n){
+        System.out.println(n.val);
+        if (n.next!= null) print(n.next);
     }
 
 }
