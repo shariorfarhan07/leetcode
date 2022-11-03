3907669224b0bf8957bc243438c0f3e68b41243b
shariorfarhan07 <md.sharior.hossain.farhan@g.bracu.ac.bd>
2022-11-03 18:52:57 +0600

    Day 6

--- src/PascalsTriangleII119.java
+++ src/PascalsTriangleII119.java
@@ -5,6 +5,16 @@ public class PascalsTriangleII119 {
     public static void main(String[] args) {
         System.out.println(new PascalsTriangleII119().getRow(3));
     }
+    public List<Integer> getRow2(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < i; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+        }
+        return row;
+    }
     public List<Integer> getRow(int rowIndex) {
 
 
