08cc8abe7eb73e7d4d4754b5690b4905f677cf9c
shariorfarhan07 <md.sharior.hossain.farhan@g.bracu.ac.bd>
2022-11-07 19:30:51 +0600

    Day 10

--- src/myStackImplementationWith2queue.java
+++ src/myStackImplementationWith2queue.java
@@ -1,27 +1,36 @@
- class MyStack {
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+        Queue<Integer>q1,q2,temp;
 
         public MyStack() {
-
+               q1=new LinkedList<>();
+               q2=new LinkedList<>();
         }
 
         public void push(int x) {
-
+                        q2.add(x);
+                        while (!q1.isEmpty())q2.add(q1.poll());
+                        temp=q1;
+                        q1=q2;
+                        q2=temp;
         }
 
         public int pop() {
 
-                return 0;
+                return q1.poll();
         }
 
         public int top() {
 
-                return 0;
+                return q1.peek();
         }
 
         public boolean empty() {
 
 
-                return false;
+                return q1.isEmpty();
         }
 
 /**
@@ -32,4 +41,29 @@
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
  */
+}
+
+/* Implementation of stack with single queue*/
+class MyStack2 {
+
+        private Queue<Integer> queue = new LinkedList<>();
+
+        public void push(int x) {
+                queue.add(x);
+                for(int i = 1;i<queue.size();i++) {
+                        queue.add(queue.remove());
+                }
+        }
+
+        public int pop() {
+                return queue.remove();
+        }
+
+        public int top() {
+                return queue.peek();
+        }
+
+        public boolean empty() {
+                return queue.isEmpty();
+        }
 }
