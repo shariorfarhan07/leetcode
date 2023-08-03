4a2ad11eed069b2f466b3d029c68a340be4c3798
shariorfarhan07 <md.sharior.hossain.farhan@g.bracu.ac.bd>
2023-08-04 03:00:36 +0600

    day 28

--- src/Remove_Duplicates_from_Sorted_Array_II.java
+++ src/Remove_Duplicates_from_Sorted_Array_II.java
@@ -1,13 +1,36 @@
+import java.util.Arrays;
+
 public class Remove_Duplicates_from_Sorted_Array_II {
 
+    public static void main(String[] args) {
+        int[] a=new int[]{1, 1, 1, 2, 2, 3, 4, 4, 5, 5, 5, 5, 6};
+        System.out.println(removeDuplicates(a));
 
-    public int removeDuplicates(int[] nums) {
-        int count=0,i=0,j=1;
-        while (i<nums.length){
+    }
+    public static int removeDuplicates(int[] nums) {
+        int count=0,i=0,j=1,zeros=0;
+        boolean isdouble=true;
+        while (i<nums.length && j<nums.length){
+        if (nums[i]==nums[j]){
+            count++;
 
+            if (count>=2 && isdouble){
+                i++;
+                isdouble=false;
+            }
+        }else {
+
+            count=0;
+            nums[i]=nums[j];
+            isdouble=true;
+
+            }
+        j++;
 
         }
 
-    return 0;
+
+
+    return zeros;
     }
 }
--- src/test/addTwoNumbers.java
+++ src/test/addTwoNumbers.java
@@ -1,28 +1,12 @@
-<<<<<<< Updated upstream
-package test;public class addTwoNumbers {
-//    public static void main(String[] args){
-//        Add_Two_Numbers.ListNode l1=new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9)));
-//        Add_Two_Numbers.ListNode l2=new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(1))))))));
-//        printlist(l1);
-//        printlist(l2);
-//        printlist(addTwoNumbers(l1,l2));
-//    }
-
-//    private static void printlist(Add_Two_Numbers.ListNode l1) {
-//
-//    }
-//    private static void addTwoNumbers(l1,l2){}
-
-}
-=======
+//<<<<<<< Updated upstream
 //package test;public class addTwoNumbers {
-//    public static void main(String[] args){
-//        Add_Two_Numbers.ListNode l1=new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9)));
-//        Add_Two_Numbers.ListNode l2=new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(1))))))));
+////    public static void main(String[] args){
+////        Add_Two_Numbers.ListNode l1=new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9)));
+////        Add_Two_Numbers.ListNode l2=new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(1))))))));
 ////        printlist(l1);
 ////        printlist(l2);
 ////        printlist(addTwoNumbers(l1,l2));
-//    }
+////    }
 //
 ////    private static void printlist(Add_Two_Numbers.ListNode l1) {
 ////
@@ -30,4 +14,20 @@ package test;public class addTwoNumbers {
 ////    private static void addTwoNumbers(l1,l2){}
 //
 //}
->>>>>>> Stashed changes
+//=======
+////package test;public class addTwoNumbers {
+////    public static void main(String[] args){
+////        Add_Two_Numbers.ListNode l1=new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9)));
+////        Add_Two_Numbers.ListNode l2=new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(9,new Add_Two_Numbers.ListNode(1))))))));
+//////        printlist(l1);
+//////        printlist(l2);
+//////        printlist(addTwoNumbers(l1,l2));
+////    }
+////
+//////    private static void printlist(Add_Two_Numbers.ListNode l1) {
+//////
+//////    }
+//////    private static void addTwoNumbers(l1,l2){}
+////
+////}
+//>>>>>>> Stashed changes
