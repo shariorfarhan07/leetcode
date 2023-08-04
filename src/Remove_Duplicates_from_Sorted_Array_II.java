e633fafa0b83932417df454079d6bf959f544190
shariorfarhan07 <md.sharior.hossain.farhan@g.bracu.ac.bd>
2023-08-05 01:31:04 +0600

    day 29

--- src/Remove_Duplicates_from_Sorted_Array_II.java
+++ src/Remove_Duplicates_from_Sorted_Array_II.java
@@ -4,33 +4,28 @@ public class Remove_Duplicates_from_Sorted_Array_II {
 
     public static void main(String[] args) {
         int[] a=new int[]{1, 1, 1, 2, 2, 3, 4, 4, 5, 5, 5, 5,5, 6};
-        System.out.println(removeDuplicates(a));
+//        System.out.println(removeDuplicates(a));
+//        a=new int[]{1,1,1,2,2,3};
+//        System.out.println(removeDuplicates(a));
+//        a=new int[]{0,0,1,1,1,1,2,3,3};
+//        System.out.println(removeDuplicates(a));
         a=new int[]{1,1,1,2,2,3};
-        System.out.println(removeDuplicates(a));
-        a=new int[]{0,0,1,1,1,1,2,3,3};
         System.out.println(removeDuplicates(a));
 
     }
     public static int removeDuplicates(int[] nums) {
-        System.out.println(Arrays.toString(nums));
-        int count=0,l=0,r=0,numbersElement=0;
-        while (l<nums.length && r<nums.length-1){
-            while (nums[r]==nums[r+1]){
-             count++;
-             r++;
-            }
-            while (l<((count<=2)? count:2)){
-                l++;
+        if (nums.length<3) return 3;
+        int count=0,l=2,r=2;
+        while (l<nums.length && r<nums.length) {
+            if (nums[r-2]==nums[r]){
+                r++;
+            }else {
+                nums[l++]=nums[r++];
             }
 
-
-
-
-
+            System.out.println(Arrays.toString(nums));
         }
-        numbersElement=i++;
-        while (i<nums.length) nums[i++]=0;
-        System.out.println(Arrays.toString(nums));
-        return numbersElement;
+        while (l<nums.length)nums[l++]=0;
+        return l;
     }
 }
