c6ee987442b66a222c186449e94b4704acd38107
shariorfarhan07 <shariorfarhan77@gmail.com>
2023-08-05 15:41:26 +0600

    day 30

--- src/Remove_Duplicates_from_Sorted_Array_II.java
+++ src/Remove_Duplicates_from_Sorted_Array_II.java
@@ -3,18 +3,37 @@ import java.util.Arrays;
 public class Remove_Duplicates_from_Sorted_Array_II {
 
     public static void main(String[] args) {
-        int[] a=new int[]{1, 1, 1, 2, 2, 3, 4, 4, 5, 5, 5, 5,5, 6};
+        int[] a=new int[]{1, 1, 1,1, 2, 2, 3, 4, 4, 5, 5, 5, 5,5, 6};
         System.out.println(removeDuplicates(a));
         a=new int[]{1,1,1,2,2,3};
         System.out.println(removeDuplicates(a));
-        a=new int[]{0,0,0,1,1,1,2,3,3,3};
+        a=new int[]{0,0,0,1,1,1,2,2,3,3,3};
         System.out.println(removeDuplicates(a));
         a=new int[]{1,1,1,2,2,3};
         System.out.println(removeDuplicates(a));
 
     }
- public static int removeDuplicates(int[] nums) {
+
+//    this one works
+    public static int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(n <= 2)
+            return n;
+        int rIndex = 1;
 //        System.out.println(Arrays.toString(nums));
+        for(int i = 1; i < n; i++)
+        {
+
+            if(nums[i] == nums[rIndex] && nums[i] == nums[rIndex - 1]) //&& nums[i] == nums[rIndex - 2])
+                continue;
+            nums[++rIndex] = nums[i];
+        }
+        System.out.println(Arrays.toString(nums));
+        return rIndex + 1;
+    }
+//    this one cant get the return index right !
+ public static int removeDuplicates2(int[] nums) {
+//     System.out.println(Arrays.toString(nums));
             if (nums.length<3) return 3;
             int count=0,l=2,r=2;
             while (l<nums.length&&r<nums.length) {
@@ -25,11 +44,11 @@ public class Remove_Duplicates_from_Sorted_Array_II {
                     if (nums[r-1]==nums[r]) l++;
                     l++;
                     r++;
-                    count++;
                 }
 
 //            System.out.println("= "+Arrays.toString(nums));
             }
+//            count=l;
             System.out.println(count);
             while (l<nums.length)nums[l++]=0;
             while (nums[--l]==0) count++;
