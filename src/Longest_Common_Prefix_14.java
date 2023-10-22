f72a83cad776fde808957291af8dba88d8802b73
shariorfarhan07 <shariorfarhan77@gmail.com>
2023-10-22 17:55:17 +0600

    day 49 updateded old code

--- src/Longest_Common_Prefix_14.java
+++ src/Longest_Common_Prefix_14.java
@@ -1,5 +1,5 @@
 public class Longest_Common_Prefix_14 {
-    public String longestCommonPrefix(String[] strs) {
+    public String longestCommonPrefix1(String[] strs) {
         int length=0;
         int stringlen=0;
         int min=9999999;
@@ -13,6 +13,24 @@ public class Longest_Common_Prefix_14 {
         }
 
 
+        return strs[0].substring(0,min);
+    }
+// feels weird  I wrote both of the codes second one is much simpler than the first one
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length==1) return strs[1];
+        int min =Integer.MAX_VALUE;
+        int minlengthoftwo=0,count=0;
+
+        for(int i = 1; i < strs.length; i++) {
+            minlengthoftwo=Math.min(strs[i-1].length() ,strs[i].length() );
+            for(int j = 0; j < minlengthoftwo; j++) {
+                if (strs[i-1].charAt(j)!=strs[i].charAt(j)) break;
+                count++;
+            }
+            min=Math.min(min,count);
+            count=0;
+        }
+        if (min==Integer.MAX_VALUE||min == 0) return "";
         return strs[0].substring(0,min);
     }
 
