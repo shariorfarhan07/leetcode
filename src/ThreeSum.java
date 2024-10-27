227a36a87f5cd1c7c2c63624e58963595e3a26cf
shariorfarhan07 <shariorfarhan77@gmail.com>
2024-10-27 17:26:24 +0600

    27/10/2024

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -5,8 +5,9 @@ import java.util.List;
 public class ThreeSum {
     public static void main(String[] args){
 //        int a[] ={0,0,0,0};
-        int a[] ={-1,0,1,2,-1,-4};
-
+        int a[] ={34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
         System.out.println(threeSum(a));
     }
 
@@ -14,10 +15,7 @@ public class ThreeSum {
         List<List<Integer>> answer=new ArrayList<>();
         Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++) {
-            if(i!=0 && nums[i]==nums[i-1]){
-                i++;
-                continue;
-            }
+            if(i!=0 && nums[i]==nums[i-1]) continue;
             int left=i+1,right=nums.length-1;
             while(left<right){
                 if (nums[i]+nums[left]+nums[right]==0){
